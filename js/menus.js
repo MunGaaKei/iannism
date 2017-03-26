bf271489ac1f110af7088ec45fefe538fd1b7524
MunGaaKei <502975759@qq.com>
2017-03-26 17:30:35 +0800

    canvas - blog

--- js/menus.js
+++ js/menus.js
@@ -5,5 +5,11 @@ var menus = [
     time: '2017-1-1',
     tag: ['随写'],
     path: '2017.html'
+  },{
+    title: 'CANVAS下的星空',
+    abstract: '使用<canvas>绘制星空图',
+    time: '2017-1-14',
+    tag: ['canvas','前端'],
+    path: 'canvas.html'
   }
 ];
