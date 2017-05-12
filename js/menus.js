c3746f55850dcb0a87687259acdaf6ee81b6d3bb
MunGaaKei <502975759@qq.com>
2017-05-12 15:59:25 +0800

    MUSIC

--- js/menus.js
+++ js/menus.js
@@ -1,21 +1,45 @@
-var menus = [
-  {
-    title: '2017',
-    abstract: '写在2017的开始',
-    time: '2017-1-1',
-    tag: ['随写'],
-    path: '2017.html'
+var menus = {
+  blogs: [
+    {
+      title: '2017',
+      abstract: '写在2017的开始',
+      time: '2017-1-1',
+      tag: ['随写'],
+      path: '2017.html'
+    },{
+      title: 'CANVAS下的星空',
+      abstract: 'canvas简易使用：绘制星空效果',
+      time: '2017-1-14',
+      tag: ['canvas','前端'],
+      path: 'canvas.html'
+    },{
+      title: '鼠标移入移出方向判断',
+      abstract: '判断鼠标移入移出方向的思路以及示例实现',
+      time: '2017-2-13',
+      tag: ['鼠标事件','前端'],
+      path: 'mouse-in-out-direction.html'
+    }
+  ],
+  photos: '',
+  musics: [{
+    file: '0.mp3',
+    song: '他夏了夏天',
+    singer: '苏打绿'
   },{
-    title: 'CANVAS下的星空',
-    abstract: 'canvas简易使用：绘制星空效果',
-    time: '2017-1-14',
-    tag: ['canvas','前端'],
-    path: 'canvas.html'
+    file: '1.mp3',
+    song: '如烟',
+    singer: '五月天'
   },{
-    title: '鼠标移入移出方向判断',
-    abstract: '判断鼠标移入移出方向的思路以及示例实现',
-    time: '2017-2-13',
-    tag: ['鼠标事件','前端'],
-    path: 'mouse-in-out-direction.html'
-  }
-];
+    file: '2.mp3',
+    song: '最后的战役',
+    singer: '周杰伦'
+  },{
+    file: '3.mp3',
+    song: '想念你',
+    singer: '冒险岛'
+  },{
+    file: '4.mp3',
+    song: '顽固',
+    singer: '五月天'
+  }]
+};
--- js/picasso.js
+++ js/picasso.js
@@ -21,7 +21,7 @@
   var menu = document.getElementById('menu'),
       input = document.querySelector('#blog input'),
       btn = input.nextElementSibling,
-      blogs = menus.concat(),
+      blogs = menus.blogs.concat(),
       blogsPerPage = 9,
       l = blogs.length,
       pageNav = document.querySelector('#blog .page-nav'),
@@ -31,7 +31,7 @@
 
   keyword.addEventListener('click', function(e){
     if(e.target.tagName.toLowerCase() != 'a') return false;
-    blogs = menus.concat();
+    blogs = menus.blogs.concat();
     printMenus(1);
     this.innerHTML = '<i>: )</i>';
   });
@@ -75,7 +75,7 @@
   function searchBlog(k, isTag){
     if(!k) return false;
     var blog, rest = [];
-    l = menus.length;
+    l = menus.blogs.length;
     blogs = [];
     k = k.toLowerCase();
     while(l--){
@@ -116,6 +116,7 @@
 
 })();
 
+// PHOTOGRAPHY INIT
 (function(){
   var h = window.innerHeight,
       c = document.getElementById('photography'),
@@ -170,7 +171,71 @@
 
 })();
 
+// MUSIC INIT
+(function(){
+  var d = document,
+      acts = d.querySelectorAll('.e-col a'),
+      audio = d.getElementById('audio'),
+      song = d.querySelector('.e-info b'),
+      singer = d.querySelector('.e-info span'),
+      progress = d.getElementById('progress'),
+      volume = d.getElementById('volume'),
+      songs = menus.musics,
+      i = 1;
 
+  acts[0].addEventListener('click', function(){
+    progress.style.width = audio.currentTime = 0;
+    play();
+  });
+  acts[2].addEventListener('click', function(){ i = cut(i); })
+  acts[3].addEventListener('click', play);
+  audio.volume = .3;
+  printMenus();
+
+  progress.parentNode.addEventListener('click', function(e){
+    progress.style.width = e.offsetX/2 + '%';
+    audio.currentTime = audio.duration * e.offsetX/200;
+  });
+  volume.parentNode.addEventListener('click', function(e){
+    volume.style.width = e.offsetX + '%';
+    audio.volume = e.offsetX/100;
+  });
+  d.getElementById('player').addEventListener('click', initSong);
+  audio.addEventListener('timeupdate',function(){ progress.style.width = audio.currentTime/audio.duration * 100 + '%'; });
+  audio.addEventListener('play',function(){ acts[3].innerHTML = '<i class="iconfont icon-stop"></i>'; });
+  audio.addEventListener('pause',function(){ acts[3].innerHTML = '<i class="iconfont icon-playfill"></i>'; });
+  audio.addEventListener('ended', function(){ i = cut(i); });
+  d.querySelector('ul.e-menu').addEventListener('click', function(e){
+    if(e.target.tagName !== 'A') return false;
+    i = cut(e.target.dataset.i);
+  });
+  d.querySelector('.e-list').addEventListener('click', function(){ d.querySelector('ul.e-menu').classList.toggle('menu-show'); });
+  function play(){ audio.paused?audio.play():audio.pause(); }
+  function cut(n){
+    song.innerHTML = songs[n].song;
+    singer.innerHTML = songs[n].singer;
+    audio.src = 'music/' + songs[n].file;
+    audio.play();
+    return n < (songs.length-1)? ++n: 0;
+  }
+  function formatMenu(n, o){ return '<li><a href="javascript:;" data-i="'+ n +'">'+ o.song +'</a> / '+ o.singer +'</li>'; }
+  function printMenus(){
+    var k = 0,
+        html = '',
+        l = songs.length;
+    for(k; k< l; k++){ html += formatMenu(k, songs[k]); }
+    d.querySelector('ul.e-menu').innerHTML = html;
+  }
+  function initSong(){
+    song.innerHTML = songs[0].song;
+    singer.innerHTML = songs[0].singer;
+    audio.src = 'music/' + songs[0].file;
+    d.getElementById('player').removeEventListener('click', initSong);
+  }
+
+})();
+
+// PUBLIC METHODS
 function showPage(p, totalpage, siblings){
   var i, pre, next,
       siblingNum = siblings? siblings: 3,
