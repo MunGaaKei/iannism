c56822c35fbc51a0bab0a56f53ced5c49addcf25
MunGaaKei <502975759@qq.com>
2017-05-24 23:54:00 +0800

    ecmasript6

--- js/menus.js
+++ js/menus.js
@@ -3,7 +3,8 @@ var menus = {
     {title: '2017',abstract: '写在2017的开始',time: '2017-1-1',tag: ['随写'],path: '2017.html'},
     {title: 'CANVAS下的星空',abstract: 'canvas简易使用：绘制星空效果',time: '2017-1-14',tag: ['canvas','JavaScript'],path: 'canvas.html'},
     {title: '鼠标移入移出方向判断',abstract: '判断鼠标移入移出方向的思路以及示例实现',time: '2017-2-13',tag: ['鼠标事件','JavaScript'],path: 'mouse-in-out-direction.html'},
-    {title: '关于一些CSS选择器',abstract: '一些相对少用的CSS选择器记录',time: '2017-4-12',tag: ['笔记','CSS'],path: 'css-selector.html'}
+    {title: '关于一些CSS选择器',abstract: '一些相对少用的CSS选择器记录',time: '2017-4-12',tag: ['笔记','CSS'],path: 'css-selector.html'},
+    {title: '接触 ECMAScript6',abstract: '关于ES6新增的一些方法的简单介绍',time: '2017-5-2',tag: ['ES6','JavaScript'],path: 'ecmascript6.html'}
   ],
   photos: '',
   musics: [
