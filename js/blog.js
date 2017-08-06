70aeb9b0bc90af79549ea8977b5c74a809c6b2e6
MunGaaKei <502975759@qq.com>
2017-08-06 14:55:32 +0800

    STYLE IMPROVE

--- js/blog.js
+++ js/blog.js
@@ -1,12 +1,12 @@
-(function(){
-	var d = document;
+;(function(){
+	var d = document,
+			header = d.getElementById('header');
 	if("ontouchstart" in d){
-		var header = d.getElementById('header');
 		header.style.position = 'absolute';
-		header.style.right = 0;
+		header.style.right = '0';
 	} else {
-	  	d.addEventListener('mousewheel', function(e){if(e.wheelDeltaY > 0){d.getElementById('header').classList.remove('hide');} else {d.getElementById('header').classList.add('hide');}});
-		d.addEventListener('DOMMouseScroll', function(e){if(e.detail < 0){d.getElementById('header').classList.remove('hide');} else {d.getElementById('header').classList.add('hide');}});
+	  d.addEventListener('mousewheel', function(e){if(e.wheelDeltaY > 0){header.classList.remove('hide');} else {header.classList.add('hide');}});
+		d.addEventListener('DOMMouseScroll', function(e){if(e.detail < 0){header.classList.remove('hide');} else {header.classList.add('hide');}});
 	}
-  	
+
 })();
