fe44937e6649edab97342eab5832a9daf59bdaf7
Ian Man <ian@mmldigi.com>
2019-07-24 17:58:23 +0800

    picasso build

--- js/picasso.js
+++ js/picasso.js
@@ -1,35 +1,5 @@
 ;(function(win, doc, undefined){
 
-	var $header = doc.getElementById('header');
-
-	$header.addEventListener('click', function(e){
-		var tar = e.target,
-			css = tar.classList;
-
-		if(css.contains('nav')){
-			var tab = doc.getElementById(tar.dataset.tab);
-			if( css.toggle('on') ){
-				var activeTab = doc.querySelector('.content.on'),
-					activeNav = doc.querySelector('.nav.on');
-				hideTab(activeTab);
-				activeNav && activeNav.classList.remove('on');
-
-				tab.style.display = 'block';
-				tab.offsetWidth;
-				tab.classList.add('on');
-			} else {
-				hideTab(tab);
-			}
-		}
-	});
-
-	function hideTab( tab ){
-		if( !tab ) return false;
-		tab.classList.remove('on');
-		setTimeout(function(){
-			tab.removeAttribute('style');
-			tab = null;
-		}, 300);
-	}
+	
 
 })(window, document);
