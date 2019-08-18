7242f156edc72f96633a13c40fbbd31f59813885
Gaakei <502975759@qq.com>
2019-08-19 01:32:46 +0800

    picasso build

--- js/picasso.js
+++ js/picasso.js
@@ -21,51 +21,6 @@
         }
     }, false);
 
-    var photograph = doc.querySelector('.photograph');
-    var imgs = photograph.querySelectorAll('img');
-    imgs[0].addEventListener('load', function(){
-        var loading = photograph.firstElementChild;
-        if( loading ){
-            loading.style.opacity = 0;
-            setTimeout(function(){
-                loading.remove();
-            }, 1000);
-        }
-    }, { once: true });
-    doc.querySelector('[data-href="p"]').addEventListener('click', function(){
-        var l = imgs.length;
-        var i = 0;
-        var img;
-        for (; i < l; i++) {
-            img = imgs[i];
-            img.setAttribute('src', img.getAttribute('data-src'));
-            img.removeAttribute('data-src');
-        }
-    }, { once: true });
-
-    photograph.addEventListener('mousewheel', throttle(function( e ){
-        var active = photograph.querySelector('.display');
-        var k = active.dataset.i - 0;
-        
-        if( e.deltaY > 0 ){
-            k = k === imgs.length - 1? 0: k + 1;
-        } else {
-            k = k === 0? imgs.length - 1: k - 1;
-        }
-
-        active.classList.remove('zoom');
-        active.classList.remove('display');
-        imgs[k].parentNode.classList.add('display');
-    }, 300));
-
-    photograph.addEventListener('click', function( e ){
-        var tar = e.target;
-        if( tar.matches('img') ){
-            var li = tar.parentNode;
-            li.classList.toggle('zoom');
-        }
-    }, true);
-
     var blog = doc.getElementById('b');
     blog.addEventListener('open', function(){
         
