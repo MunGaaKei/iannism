d96c16bb4142ba88ba9d1ca2a4b0553c05ca23f8
Ian Man <ian@mmldigi.com>
2019-08-22 18:04:27 +0800

    picasso build

--- js/picasso.js
+++ js/picasso.js
@@ -10,18 +10,8 @@
 	doc.addEventListener('click', function( e ){
         var tar = e.target;
         if( tar.matches('[data-href]') && !cantAnimate ){
-            var room = doc.getElementById(tar.dataset.href);
-            var curr = doc.querySelector('.room.active');
-            curr.classList.remove('active');
-            cantAnimate = setTimeout(function(){
-                curr.classList.remove('on');
-                curr.dispatchEvent(evts.close);
-                cantAnimate = false;
-            }, 300);
-            room.dispatchEvent(evts.open);
-            room.classList.add('on');
-            room.offsetWidth;
-            room.classList.add('active');
+            close( doc.querySelector('.room.active') );
+            open( doc.getElementById(tar.dataset.href) );
         }
     }, false);
 
@@ -45,13 +35,36 @@
     });
 
     var photograhp = doc.getElementById('p');
+    photograhp.addEventListener('open', function(){
+        lazyload( this.querySelectorAll('img') );
+    });
     photograhp.addEventListener('click', function( e ){
         var tar = e.target;
         if( tar.matches('img') ){
-            console.log(tar);
-            // tar.complete;
+            e.stopPropagation();
+            var iw = win.innerWidth;
+            if( iw > 860 ) {
+                var frame = doc.getElementById('f');
+                var framer = frame.querySelector('.framer img');
+                framer.setAttribute('src', tar.getAttribute('src'));
+                close( this );
+                open( frame );
+            } else {
+                var li = tar.parentNode;
+                if( li.classList.toggle('expand') ){
+                    var ht = tar.offsetHeight;
+                    li.style.cssText = 'height:'+ ht +'px';
+                } else {
+                    li.removeAttribute('style');
+                }
+                
+            }
+        } else {
+            close( this );
+            open( doc.getElementById('h') );
         }
-    }, true);
+    });
+
 
     /* 节流封装 */
     function throttle( fn, ms ){
@@ -68,6 +81,35 @@
     			setTimeout(function(){ f = true; }, ms);
     		}
     	}
+    }
+
+    function close( el ) {
+        el.classList.remove('active');
+        cantAnimate = setTimeout(function(){
+            el.classList.remove('on');
+            el.dispatchEvent(evts.close);
+            cantAnimate = false;
+        }, 300);
+    }
+
+    function open( el ){
+        el.dispatchEvent(evts.open);
+        el.classList.add('on');
+        el.offsetWidth;
+        el.classList.add('active');
+    }
+
+    function lazyload( imgs ){
+        var l = imgs.length;
+        var i = 0;
+        var img;
+        for(; i < l; i++){
+            img = imgs[i];
+            img.setAttribute('src', img.dataset.src);
+            img.addEventListener('load', function(){
+                this.classList.remove('waiting');
+            }, { once: true });
+        }
     }
 
 })(window, document);
