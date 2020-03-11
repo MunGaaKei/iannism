a93955e6a1c5f0c80c3e7be27fdf84867457048d
Ian Man <ian@mmldigi.com>
2020-03-11 18:04:51 +0800

    picasso build

--- js/article.js
+++ js/article.js
@@ -4,11 +4,11 @@
         let tar = entries[0].target;
         if( tar.classList.contains('img') ) return;
         tar.setAttribute('src', tar.dataset.src);
-        tar.parentNode.dataset.alt = `Image: ${tar.alt}`;
+        tar.parentNode.dataset.alt = ` ${tar.alt}`;
     });
 
     Array.from( doc.getElementsByClassName('img-wait') ).forEach( $div => {
-        let $img = $div.firstElementChild;
+        let $img = $div.lastElementChild;
         $img.addEventListener('load', () => {
             $img.className = 'img';
             $div.removeAttribute('class');
